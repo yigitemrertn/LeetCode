c0a7184ace1de8873727b2b9044f818b243b59c7
Yiğit Emre <yigitemreerten@gmail.com>
2025-03-11 09:08:34 +0300

    Deneme

--- aasdasd.java
+++
@@ -1,9 +0,0 @@
-import java.util.Arrays;
-
-public class aasdasd {
-public static void main(String[] args) {
-    boolean[] sex = {true, false, true, false, true, false, true, false, true, false};
-    System.out.println(Arrays.toString(sex));
-}
-
-}
